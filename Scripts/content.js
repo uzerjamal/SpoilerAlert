31bfdddb3ecb0fa012efe60759de80e637c16b24
Uzer Jamal <uzerjamal@gmail.com>
2021-01-22 23:46:26 +0530

    Added hide image functionality

--- Scripts/content.js
+++ Scripts/content.js
@@ -1,5 +1,5 @@
-const textElements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, span, a, p, li');
-let keywords = ['the walking dead', 'walking dead'];
+const elements = document.querySelectorAll('h1, h2, h3, h4, h5, h6, span, a, p, li, img');
+let keywords = ['the walking dead', 'walking dead', 'TWD'];
 
 const checkKeywords = (str) => {
     for (let i=0; i<keywords.length; i++){
@@ -8,15 +8,21 @@ const checkKeywords = (str) => {
     return false;
 }
 
-for(let i=0; i<textElements.length; i++){
-    if(checkKeywords(textElements[i].innerText.toLowerCase())){
-        textElements[i].setAttribute('style', '-webkit-filter: blur(5px)');
+for(let i=0; i<elements.length; i++){
+    if(elements[i].nodeName === 'IMG'){
+        if(checkKeywords(elements[i].alt))
+            elements[i].setAttribute('style', '-webkit-filter: blur(5px)');
     }
+    else {
+        if(checkKeywords(elements[i].innerText.toLowerCase())){
+            elements[i].setAttribute('style', '-webkit-filter: blur(5px)');
+        }
+    }
+
 }
 
 
-
-    // const win = window.onload = () => {
-    //     let comic = $(`:contains("comic"):not(:has(:contains("comic")))`);
-    //     comic.css( "text-decoration", "underline" )
-    // }
+//const win = window.onload = () => {
+//    let comic = $(`:contains("comic"):not(:has(:contains("comic")))`);
+//    comic.css( "text-decoration", "underline" )
+//}
