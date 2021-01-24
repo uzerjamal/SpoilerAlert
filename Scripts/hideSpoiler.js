0994b56745b1697bfeeeeb6aa9574982ce5dd323
Uzer Jamal <uzerjamal@gmail.com>
2021-01-24 13:21:28 +0530

    Added base structure for displaying search results

--- Scripts/hideSpoiler.js
+++ Scripts/hideSpoiler.js
@@ -2,6 +2,14 @@ const apiKey = '96599eda9024c9c8ccf18ccdd4488bfe' //TMDB api key
 const resultList = document.getElementById('list');
 const pendingListItem = document.getElementById('pending');
 
+const displayResults = (resultsArray) => {
+    for(let i=0; i<4; i++){
+        document.getElementById(`img${i+1}`).src = resultsArray[i].posterPath;
+        document.getElementById(`title${i+1}`).innerHTML = resultsArray[i].title;
+    }
+    document.getElementById('content-area-id').style.display = 'block';
+}
+
 const getTitles = (title) => {
     let titlesArray = [];
     const queryUrl = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}`+
@@ -10,11 +18,11 @@ const getTitles = (title) => {
         .then(response => response.json())
         .then(data => {
             pendingListItem.style.display = 'none';
-            for(let i=0; i<5; i++){
+            for(let i=0; i<4; i++){
                 titlesArray[i]= {
                     'id': data.results[i].id,
                     'mediaType': data.results[i].media_type,
-                    'posterPath': 'http://image.tmdb.org/t/p/w45' + data.results[i].poster_path,
+                    'posterPath': 'http://image.tmdb.org/t/p/w92' + data.results[i].poster_path,
                 };
                 if(titlesArray[i].mediaType === 'movie'){
                     titlesArray[i].title = data.results[i].title;
@@ -24,7 +32,8 @@ const getTitles = (title) => {
                     titlesArray[i].releaseDate = data.results[i].first_air_date;
                 }
             }
-            console.log(titlesArray);
+            //console.log(titlesArray);
+            displayResults(titlesArray);
         });
     pendingListItem.style.display = 'block'; //executes when promise is pending
 }
